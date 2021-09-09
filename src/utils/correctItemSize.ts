8807ffcf0614ed626773355f713afb07b4393cca
Aleksandar Ivanov <alex9408@icloud.com>
2021-09-09 16:39:14 +0300

    Update correctItemSize.ts
    temp work arround

--- src/utils/correctItemSize.ts
+++ src/utils/correctItemSize.ts
@@ -1,3 +1,3 @@
 export function correctItemSize(el: HTMLElement, dimension: 'height' | 'width') {
-  return Math.round(el.getBoundingClientRect()[dimension])
+  return Math.round(el?.getBoundingClientRect()[dimension])
 }
